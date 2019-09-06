4842100377fc21c0fa3a296e6d6c7f8e7d55c523
khurramHashmi <khurramhashim94@gmail.com>
2019-09-06 19:08:55 +0200

    Table detection usig Mask_rcnn with all the scripts

---
+++ demo.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Mask R-CNN Demo
+# 
+# A quick intro to using the pre-trained model to detect and segment objects.
+
+# In[1]:
+
+
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+# Import COCO config
+sys.path.append(os.path.join(ROOT_DIR, "samples/table/"))  # To find local version
+import table
+
+#get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "samples/table/logs/latet_model_50_epochs/mask_rcnn_table_0050.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "samples/table/custom_data_set/test/")
+
+
+# ## Configurations
+# 
+# We'll be using a model trained on the MS-COCO dataset. The configurations of this model are in the ```CocoConfig``` class in ```coco.py```.
+# 
+# For inferencing, modify the configurations a bit to fit the task. To do so, sub-class the ```CocoConfig``` class and override the attributes you need to change.
+
+# In[2]:
+
+
+class InferenceConfig(table.TableConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+
+# ## Create Model and Load Trained Weights
+
+# In[3]:
+
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+
+# ## Class Names
+# 
+# The model classifies objects and returns class IDs, which are integer value that identify each class. Some datasets assign integer values to their classes and some don't. For example, in the MS-COCO dataset, the 'person' class is 1 and 'teddy bear' is 88. The IDs are often sequential, but not always. The COCO dataset, for example, has classes associated with class IDs 70 and 72, but not 71.
+# 
+# To improve consistency, and to support training on data from multiple sources at the same time, our ```Dataset``` class assigns it's own sequential integer IDs to each class. For example, if you load the COCO dataset using our ```Dataset``` class, the 'person' class would get class ID = 1 (just like COCO) and the 'teddy bear' class is 78 (different from COCO). Keep that in mind when mapping class IDs to class names.
+# 
+# To get the list of class names, you'd load the dataset and then use the ```class_names``` property like this.
+# ```
+# # Load COCO dataset
+# dataset = coco.CocoDataset()
+# dataset.load_coco(COCO_DIR, "train")
+# dataset.prepare()
+# 
+# # Print class names
+# print(dataset.class_names)
+# ```
+# 
+# We don't want to require you to download the COCO dataset just to run this demo, so we're including the list of class names below. The index of the class name in the list represent its ID (first class is 0, second is 1, third is 2, ...etc.)
+
+# In[4]:
+
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'table']
+
+
+# ## Run Object Detection
+
+# In[5]:
+
+
+# Load a random image from the images folder
+file_names = next(os.walk(IMAGE_DIR))[2]
+print(file_names)
+for file in file_names:
+  image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+  image = skimage.color.gray2rgb(image, alpha=None)  #covering Image in readable format for model
+  # Run detection
+  results = model.detect([image], verbose=1)
+  # Visualize results
+  r = results[0]
+  print(r['rois'])
+  visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+                              class_names, r['scores'])
+
+
+# In[ ]:
+
+
+
+
